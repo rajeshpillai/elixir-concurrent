7141caa0c05f841d932eb6fa7e57e73e5322ddf3
rajeshpillai <pillai.rajesh@gmail.com>
2022-09-09 11:13:23 +0530

    Steps to execute
    Start the GenServer
    1.  {:ok, pid} = GenServer.start(SendServer, [max_retries: 1])
    
    Stop the genserver
    2.  GenServer.stop(pid)

--- sender/lib/send_server.ex
+++ sender/lib/send_server.ex
@@ -5,6 +5,7 @@ defmodule SendServer do
   # It automatically injects the @behaviour GenServer line in our SendServer module. If you’re not familiar with behaviours in Elixir, they’re similar to interfaces and contracts in other programming languages.
   # It also provides a default GenServer implementation by injecting all functions required by the GenServer behaviour.
 
+  # ADD executable steps in each commit
   def init(args) do
     IO.puts("Received arguments: #{inspect(args)}")
     max_retries = Keyword.get(args, :max_retries, 5)
